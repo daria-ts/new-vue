f0a062a0fdde5fabe6a61c61842a61e48725506e
daria <tochdz@gmail.com>
2021-02-08 22:28:37 +0300

    delete svg icon

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,39 +1,3 @@
-const svgFilePath = "svg file path （absolute path)";
-{
-	chainWebpack(config);
-	{
-		config.module
-			.rule("vue-svgicon")
-			.include.add(svgFilePath)
-			.end()
-			.test(/\.svg$/)
-			.use("svgicon")
-			.loader("@yzfe/svgicon-loader")
-			.options({
-				svgFilePath,
-			});
-
-		config.module
-			.rule("svg")
-			.exclude.add(svgFilePath)
-			.end();
-
-		// Recommended configuration transformAssetUrls
-		config.module
-			.rule("vue")
-			.use("vue-loader")
-			.loader("vue-loader")
-			.tap((opt) => {
-				opt.transformAssetUrls = opt.transformAssetUrls || {};
-				opt.transformAssetUrls["icon"] = ["data"];
-				return opt;
-			});
-
-		// Recommended configuration alias
-		config.resolve.alias.set("@icon", svgFilePath);
-	}
-}
-
 module.exports = {
 	pwa: {
 		themeColor: "#ff1129",
