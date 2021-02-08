ccd7aebad3b3199dc9bd632d5263f7e5f9b90756
daria <tochdz@gmail.com>
2021-02-08 21:11:49 +0300

    vuesvgicon fixed

--- src/main.js
+++ src/main.js
@@ -4,15 +4,13 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import "./global-styles/colors.scss";
-
 // import "./global-styles/typography.scss";
 import "./global-styles/tokens.scss";
-
 import { VueSvgIconPlugin } from "@yzfe/vue3-svgicon";
 import "@yzfe/svgicon/lib/svgicon.css";
 
 createApp(App)
-  .use(VueSvgIconPlugin, { tagName: "icon-name" })
-  .use(store)
-  .use(router)
-  .mount("#app");
+	.use(VueSvgIconPlugin, { tagName: "icon-name" })
+	.use(store)
+	.use(router)
+	.mount("#app");
