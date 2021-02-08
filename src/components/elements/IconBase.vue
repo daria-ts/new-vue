d43e42b9ad350a01a8cbee7405cdc0863ad39169
daria <tochdz@gmail.com>
2021-02-08 22:53:12 +0300

    delete from reg

--- src/components/elements/IconBase.vue
+++ src/components/elements/IconBase.vue
@@ -3,7 +3,7 @@
     xmlns="http://www.w3.org/2000/svg"
     :width="width"
     :height="height"
-    viewBox="0 0 24 24"
+    :viewBox="viewBox"
     :aria-labelledby="iconName"
     role="presentation"
   >
@@ -17,22 +17,11 @@
 <script>
 export default {
   props: {
-    iconName: {
-      type: String,
-      default: "box"
-    },
-    width: {
-      type: [Number, String],
-      default: 24
-    },
-    height: {
-      type: [Number, String],
-      default: 24
-    },
-    iconColor: {
-      type: String,
-      default: "currentColor"
-    }
+    viewBox: { type: String, default: '0 0 24 24'},
+    iconName: { type: String, default: ''},
+    width: { type: [Number, String], default: 18 },
+    height: {type: [Number, String], default: 18 },
+    iconColor: {type: String, default: 'currentColor'}
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -6,11 +6,10 @@ import store from "./store";
 import "./global-styles/colors.scss";
 // import "./global-styles/typography.scss";
 import "./global-styles/tokens.scss";
-import { VueSvgIconPlugin } from "@yzfe/vue3-svgicon";
-import "@yzfe/svgicon/lib/svgicon.css";
+import IconBase from "@/components/IconBase.vue";
+Vue.component("icon-base", IconBase);
 
 createApp(App)
-	.use(VueSvgIconPlugin, { tagName: "icon-name" })
 	.use(store)
 	.use(router)
 	.mount("#app");
