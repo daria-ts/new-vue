00fb712788e7c5f291b70a7c9da32bddb7e4cec0
daria <tochdz@gmail.com>
2021-02-10 16:49:01 +0300

    commented states for button

--- src/components/elements/ButtonBase.vue
+++ src/components/elements/ButtonBase.vue
@@ -26,11 +26,11 @@ export default {
 @import "@/global-styles/styles.scss";
 
 button {
-    @include button-l;
-    @include btn-h;
-    @include btn-priority-extra;
+    @include button-l; //button-l -- for large, button-s for small
+    @include btn-h; //btn-p -- for small, btn-h -- for large
+    @include btn-priority-extra; // -extra (primary), -hight (dark), -normal(white, primary txt), -low (white, gray txt)
 }
-
+//TODO: из миксинов не работают -- яхз но может решу потом
 button:hover {
     box-shadow: $btn-shadow-inner;
 }
@@ -47,7 +47,7 @@ button:focus {
     outline-offset: 0;
 }
 
-//выравнивание иконок
+//выравнивание иконок -- if icon visible -- comment display (пока)
 .icon-r {
     margin-left: 0.5rem;
     display: none;
