9fc53582f40bed852d4cf11ed341ff9fcf55509c
daria <tochdz@gmail.com>
2021-02-10 10:57:34 +0300

    login-form-links commented

--- src/components/elements/FormCardLinks.vue
+++ src/components/elements/FormCardLinks.vue
@@ -1,21 +1,44 @@
 <template>
     <div class="link-block">
-<router-link  :to="{ name: 'recover' }">Не помню пароль</router-link>
-<router-link :to="{ name: 'register' }">Регистрация</router-link>
+        <router-link class="caption" :to="{ name: 'recover' }">{{recoverTxt}}</router-link>
+        <router-link class="caption" :to="{ name: 'register' }">{{registerTxt}}</router-link>
     </div>
 </template>
+// TODO: <router-link class="caption" :to="{ name: 'login' }">{{recoverTxt}}</router-link> 
 
 <script>
 export default {
+        data() {
+    return {
+        recoverTxt:'Не помню пароль',
+        registerTxt:'Регистрация',
+        loginTxt:'Войти',
+    }
+},
+
     name: "FormCardLinks"
 }
 </script>
 
+
+//TODO: сделать смену блока -- в зависимости от страницы и разный текст местами типа if current page '/'name === link-name hide link
+
 <style lang="scss" scoped>
-/* .link-block{
+@import "@/global-styles/colors.scss";
+@import "@/global-styles/tokens.scss";
+.link-block{
     display: flex;
+    width: 14.75rem;
     justify-content: space-between;
-    align-items: center;
-} */
+    
+}
+//FIXME: не работают стили наведения
+.caption {
+    text-decoration: none;
+    color: $primary-500;
+    :hover {
+        text-decoration-line: underline;
+    }
+}
 
 </style>
